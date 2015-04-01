0b9abf72e7606bdb301b4e0b4706725448edf1e3
Bryan Finlayson <bdfinlayson@gmail.com>
2015-04-01 10:13:10 -0500

    submit solution to palindrome kata

---
+++ palindromes.js
@@ -0,0 +1,12 @@
+A palindrome is a string which reads the same forward and backward. It can be a word or a sentence. Examples are:
+
+abba
+Mom!
+Dad?
+If I had a hi-fi...
+Create a function which determines for a given string if it's a palidrome or not. For this task the following properties must be fulfilled:
+
+return a boolean value
+consider an empty string as a legal word
+ignore case
+ignore whitespace and punctuation
