e9005fd329f6a3b71ed72888405264852df4b2a7
Bryan Finlayson <bdfinlayson@gmail.com>
2015-04-01 10:14:35 -0500

    submit solution to palindrome kata

--- palindromes.js
+++ palindromes.js
@@ -1,12 +1,35 @@
-A palindrome is a string which reads the same forward and backward. It can be a word or a sentence. Examples are:
+// A palindrome is a string which reads the same forward and backward. It can be a word or a sentence. Examples are:
+//
+// abba
+// Mom!
+// Dad?
+// If I had a hi-fi...
+// Create a function which determines for a given string if it's a palidrome or not. For this task the following properties must be fulfilled:
+//
+// return a boolean value
+// consider an empty string as a legal word
+// ignore case
+// ignore whitespace and punctuation
 
-abba
-Mom!
-Dad?
-If I had a hi-fi...
-Create a function which determines for a given string if it's a palidrome or not. For this task the following properties must be fulfilled:
+// Answer:
 
-return a boolean value
-consider an empty string as a legal word
-ignore case
-ignore whitespace and punctuation
+function isPalindrome(str) {
+  //take word and remove all punctuation and whitespace
+  str = str.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()\'\s]/g,"");
+  //then split it into an array, reverse it, make it lower case, and compare it
+  if (str.split('').reverse().join('').toLowerCase() === str.split('').join('').toLowerCase()) {
+    console.log('is true');
+    return true;
+  } else {
+    console.log('is false');
+    return false;
+  }
+}
+
+
+// Test Cases:
+
+Test.assertEquals(isPalindrome(""), true);
+Test.assertEquals(isPalindrome("maoam"), true);
+Test.assertEquals(isPalindrome("abc"), false);
+Test.assertEquals(isPalindrome("If I had a hi-fi..."), true);
