184c36b9b6d0a3103d4ba72ce63dc5d170712f84
Bryan Finlayson <bdfinlayson@gmail.com>
2015-04-04 10:28:27 -0500

    Add solution to westerosi-houses kata

--- westerosi-houses.js
+++ westerosi-houses.js
@@ -29,8 +29,16 @@ var targaryen = {name: "Targaryen", motto: "Fire and Blood"};
 // Implement the function
 function motto(name) {
   // Search the Houses array for a house whose name
-  // equals the name passed to motto() and return it
-  // If none are found, return an empty string.
+  houses.forEach(function(e, i){
+    var houseName = houses[i].name;
+    if (houseName === name) {
+      // equals the name passed to motto() and return it
+      return houses[i].motto;
+    } else {
+      // If none are found, return an empty string.
+      return "";
+    }
+  });
 }
 
 
@@ -42,3 +50,19 @@ function motto(name) {
 //  Test.assertEquals(actual, expected, [optional] message)
 //  Test.assertSimilar(actual, expected, [optional] message)
 //  Test.assertNotEquals(actual, expected, [optional] message)
+Test.expect(houses.length < 11, "Too many houses.");
+Test.expect(houses.length == 10, "Too few houses.");
+Test.expect(houses[0].name, "Houses must have a name.");
+Test.expect(houses[0].motto, "Houses must have a motto.");
+
+Test.expect(motto("derp") == "", "When passed an invalid House name, motto should return an empty string");
+Test.expect(motto("Targaryen") == "Fire and Blood", "The motto for House Targaryen is incorrect.");
+Test.expect(motto("Stark") == "Winter is Coming", "The motto for House Stark is incorrect.");
+Test.expect(motto("Bolton") == "Our Blades Are Sharp", "The motto for House Bolton is incorrect.");
+Test.expect(motto("Greyjoy") == "We Do Not Sow", "The motto for House Greyjoy is incorrect.");
+Test.expect(motto("Tully") == "Family, Duty, Honor", "The motto for House Tully is incorrect.");
+Test.expect(motto("Arryn") == "As High as Honor", "The motto for House Arryn is incorrect.");
+Test.expect(motto("Lannister") == "Hear Me Roar!", "The motto for House Lannister is incorrect.");
+Test.expect(motto("Tyrell") == "Growing Strong", "The motto for House Tyrell is incorrect.");
+Test.expect(motto("Baratheon") == "Ours is the Fury", "The motto for House Baratheon is incorrect.");
+Test.expect(motto("Martell") == "Unbowed, Unbent, Unbroken", "The motto for House Martell is incorrect.");
