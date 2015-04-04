04e7cc52532356921c66ff5146effda83ca6cea1
Bryan Finlayson <bdfinlayson@gmail.com>
2015-04-03 22:56:57 -0500

    add westerosi_houses kata. Task is to display the respective mottos of westerosi houses

---
+++ westerosi-houses.js
@@ -0,0 +1,44 @@
+// Given a list of the following major Houses of Westeros and their respective mottos:
+//
+// var houses = [
+//   {name: "Targaryen", motto: "Fire and Blood"},
+//   {name: "Stark",     motto: "Winter is Coming"},
+//   {name: "Bolton",    motto: "Our Blades Are Sharp"},
+//   {name: "Greyjoy",   motto: "We Do Not Sow"},
+//   {name: "Tully",     motto: "Family, Duty, Honor"},
+//   {name: "Arryn",     motto: "As High as Honor"},
+//   {name: "Lannister", motto: "Hear Me Roar!"},
+//   {name: "Tyrell",    motto: "Growing Strong"},
+//   {name: "Baratheon", motto: "Ours is the Fury"},
+//   {name: "Martell",   motto: "Unbowed, Unbent, Unbroken"}
+// ];
+// Write a function that, when passed the name of a House, returns its motto. For instance:
+//
+// motto("Tyrell")
+// should return
+//
+// "growing strong"
+// If passed an invalid House name, the script should return an empty string.
+
+// Your Solution:
+
+// Add the Houses to the array
+var houses = [];
+var targaryen = {name: "Targaryen", motto: "Fire and Blood"};
+
+// Implement the function
+function motto(name) {
+  // Search the Houses array for a house whose name
+  // equals the name passed to motto() and return it
+  // If none are found, return an empty string.
+}
+
+
+
+// Your Test Cases:
+
+// Create your own tests here. These are some of the methods available:
+//  Test.expect(boolean, [optional] message)
+//  Test.assertEquals(actual, expected, [optional] message)
+//  Test.assertSimilar(actual, expected, [optional] message)
+//  Test.assertNotEquals(actual, expected, [optional] message)
